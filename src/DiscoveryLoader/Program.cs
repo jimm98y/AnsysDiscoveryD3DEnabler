4aa196092914b83c9f4a41eb831a379a9d1ed6dc
Lukas Volf <jimm98y@hotmail.com>
2025-06-21 23:13:51 +0200

    R Refactoring of the path resolution

--- src/DiscoveryLoader/Program.cs
+++ src/DiscoveryLoader/Program.cs
@@ -16,38 +16,34 @@ namespace DiscoveryLoader
                 if (e.LoadedAssembly.FullName.Contains("StrideWrapper"))
                 {
                     // force Direct3D11 rendering mode
-                    var cmdLineInstance = Assembly.LoadFrom(Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "UI Foundation.dll")).GetType("SpaceClaim.CommandLine").GetProperty("Instance", BindingFlags.Static | BindingFlags.Public).GetValue(null, null);
+                    var cmdLineInstance = Assembly.LoadFrom(ResolvePath("UI Foundation.dll")).GetType("SpaceClaim.CommandLine").GetProperty("Instance", BindingFlags.Static | BindingFlags.Public).GetValue(null, null);
                     cmdLineInstance.GetType().GetProperty("Renderer").SetValue(cmdLineInstance, 3); // Direct3D11
                 }
                 else if (e.LoadedAssembly.FullName.Contains("Disco.CoreUI"))
                 {
                     // disable startup compatibility error checks
-                    var cmdLineArgs = Assembly.LoadFrom(Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "Disco.Application.dll")).GetType("Ansys.Disco.CommandLine.CommandLineArgs").GetMethod("Parse", BindingFlags.Static | BindingFlags.Public).Invoke(null, new object[] { args });
+                    var cmdLineArgs = Assembly.LoadFrom(ResolvePath("Disco.Application.dll")).GetType("Ansys.Disco.CommandLine.CommandLineArgs").GetMethod("Parse", BindingFlags.Static | BindingFlags.Public).Invoke(null, new object[] { args });
                     cmdLineArgs.GetType().GetProperty("SkipHardwareCompatibilityChecks", BindingFlags.Instance | BindingFlags.Public).SetValue(cmdLineArgs, true);
                     cmdLineArgs.GetType().GetProperty("SkipPrerequisiteChecks", BindingFlags.Instance | BindingFlags.Public).SetValue(cmdLineArgs, true);
                     cmdLineArgs.GetType().GetProperty("Configuration", BindingFlags.Instance | BindingFlags.Public).SetValue(cmdLineArgs, 2); // Modeling only
-                    
-                    var cmdlineValue = Type.GetType("System.Windows.Application, PresentationFramework, Version=4.0.0.0, Culture=neutral, PublicKeyToken=31bf3856ad364e35").GetProperty("Current", BindingFlags.Static | BindingFlags.Public).GetValue(null, null);
-                    cmdlineValue.GetType().BaseType.GetField("_commandLineArgs", BindingFlags.NonPublic | BindingFlags.Instance).SetValue(cmdlineValue, cmdLineArgs);
+                    var cmdLineInstance = Type.GetType("System.Windows.Application, PresentationFramework, Version=4.0.0.0, Culture=neutral, PublicKeyToken=31bf3856ad364e35").GetProperty("Current", BindingFlags.Static | BindingFlags.Public).GetValue(null, null);
+                    cmdLineInstance.GetType().BaseType.GetField("_commandLineArgs", BindingFlags.NonPublic | BindingFlags.Instance).SetValue(cmdLineInstance, cmdLineArgs);
                 }
             };
-
             // use binding redirects from Discovery.exe.config
-            var assemblyBinding = XDocument.Load(Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "Discovery.exe.config")).Root.Element("runtime").Elements(XName.Get("assemblyBinding", "urn:schemas-microsoft-com:asm.v1"));
+            var assemblyBinding = XDocument.Load(ResolvePath("Discovery.exe.config")).Root.Element("runtime").Elements(XName.Get("assemblyBinding", "urn:schemas-microsoft-com:asm.v1"));
             AppDomain.CurrentDomain.AssemblyResolve += (o, e) =>
             {
                 var requestedAssembly = new AssemblyName(e.Name);
                 var dependentAssemblies = assemblyBinding.Select(x => x.Descendants(XName.Get("dependentAssembly", "urn:schemas-microsoft-com:asm.v1"))).Where(x => x.Elements(XName.Get("assemblyIdentity", "urn:schemas-microsoft-com:asm.v1")).Attributes(XName.Get("name")).First().Value == requestedAssembly.Name).SingleOrDefault();
-                if (dependentAssemblies == null)
-                    return null;
-
+                if (dependentAssemblies == null) return null;
                 var forwardedVersion = dependentAssemblies.Elements(XName.Get("bindingRedirect", "urn:schemas-microsoft-com:asm.v1")).Attributes(XName.Get("newVersion")).First().Value;
                 requestedAssembly.Version = new Version(forwardedVersion);
                 return Assembly.Load(requestedAssembly);
             };
-
             // start the application
-            Assembly.LoadFrom(Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "Discovery.exe")).GetType("Ansys.Disco.Program").GetMethod("Main").Invoke(null, new object[] { args });
+            Assembly.LoadFrom(ResolvePath("Discovery.exe")).GetType("Ansys.Disco.Program").GetMethod("Main").Invoke(null, new object[] { args });
         }
+        private static string ResolvePath(string fileName) { return Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), fileName); }
     }
 }
